c0ed9849be4ed9e2d3c36c5e50503e7fa3015027
zhaojing <zhaojing@hualala.com>
2018-06-29 16:04:38 +0800

    first

---
+++ pages/home/index.js
@@ -0,0 +1,30 @@
+const app = getApp();
+Page({
+    data: {
+        msg: 'nihao',
+    },
+    onLoad: function(options) {
+        //Do some initialize when page load.
+        
+    },
+    onReady: function() {
+        //Do some when page ready.
+        
+    },
+    onShow: function() {
+        //Do some when page show.
+        
+    },
+    onHide: function() {
+        //Do some when page hide.
+        
+    },
+    onUnload: function() {
+        //Do some when page unload.
+        
+    },
+    onPullDownRefresh: function() {
+        //Do some when page pull down.
+        
+    }
+})
